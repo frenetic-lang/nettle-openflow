df77b9ce6f6c68d1bfc3bbfa787c8b7f4881db26
Arjun Guha <arjun.guha@gmail.com>
2012-09-25 12:28:51 -0400

    copied from Frenetic repo. These belong here.

---
+++ src/Nettle/Arbitrary.hs
@@ -0,0 +1,74 @@
+module Nettle.Arbitrary
+  (
+  ) where
+
+import Nettle.Ethernet.EthernetAddress
+import Nettle.IPv4.IPAddress
+import Nettle.OpenFlow
+import Test.QuickCheck
+
+instance Arbitrary IPAddress where
+  arbitrary = do
+    addr <- arbitrary
+    return $ IPAddress addr
+
+instance Arbitrary IPAddressPrefix where
+  arbitrary = do
+    addr <- arbitrary
+    len <- oneof $ map return [0 .. 32 ]
+    return (IPAddressPrefix addr len)
+
+instance Arbitrary EthernetAddress where
+  arbitrary = do
+    w1 <- arbitrary
+    w2 <- arbitrary
+    w3 <- arbitrary
+    w4 <- arbitrary
+    w5 <- arbitrary
+    w6 <- arbitrary
+    return $ ethernetAddress w1 w2 w3 w4 w5 w6
+
+instance Arbitrary Match where
+  arbitrary = do
+    inPort_v <- arbitrary
+    srcEthAddress_v <- arbitrary
+    dstEthAddress_v <- arbitrary
+    vLANID_v <- arbitrary
+    vLANPriority_v <- arbitrary
+    ethFrameType_v <- arbitrary
+    ipTypeOfService_v <- arbitrary
+    ipProtocol_v <- arbitrary
+    srcIPAddress_v <- arbitrary
+    dstIPAddress_v <- arbitrary
+    srcTransportPort_v <- arbitrary
+    dstTransportPort_v <- arbitrary
+    return $ Match {
+        inPort = inPort_v
+      , srcEthAddress = srcEthAddress_v
+      , dstEthAddress = dstEthAddress_v
+      , vLANID = vLANID_v
+      , vLANPriority = vLANPriority_v
+      , ethFrameType = ethFrameType_v
+      , ipTypeOfService = ipTypeOfService_v
+      , matchIPProtocol = ipProtocol_v
+      , srcIPAddress = srcIPAddress_v
+      , dstIPAddress = dstIPAddress_v
+      , srcTransportPort = srcTransportPort_v
+      , dstTransportPort = dstTransportPort_v
+      }
+
+{-
+instance Arbitrary Action where
+-- TODO: generate modification actions
+  arbitrary = do
+    p <- arbitrary
+    return $ SendOutPort p
+
+instance Arbitrary PseudoPort where
+  arbitrary = do
+    let maxLenToSendController = 65535
+    p <- arbitrary
+    oneof [ return $ PhysicalPort p
+          , return Flood
+          ]
+-}
